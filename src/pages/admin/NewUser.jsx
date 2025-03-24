f85df73777c620631a30eb1ad515e5a1e503d7fa
ssanchezdevs <ssanchezdevs@gmail.com>
2025-03-24 18:22:15 -0300

    user

---
+++ src/pages/admin/NewUser.jsx
@@ -0,0 +1,10 @@
+const NewUser = () => {
+  return (
+    <div className="card p-3">
+      <h3>Nuevo usuario</h3>
+      <p>Formulario para crear un nuevo usuario.</p>
+    </div>
+  );
+};
+
+export default NewUser;
---
+++ src/pages/admin/UserList.jsx
@@ -0,0 +1,10 @@
+const UserList = () => {
+  return (
+    <div className="card p-3">
+      <h3>Listado de usuarios</h3>
+      <p>Aquí se muestra el listado...</p>
+    </div>
+  );
+};
+
+export default UserList;
